eb7b1a7f4e9504efce655f3b1afc2b3b702f26ca
greenmuffins <kht.ngo@gmail.com>
2015-05-02 19:35:47 -0400

    final countdown push

--- freelancers/board/forms.py
+++ freelancers/board/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.db import models
-from models import Posting, Response, Upload, Item
+from board.models import Posting, Response, Upload, Item
 
 class PostingForm(forms.ModelForm):
     class Meta:
